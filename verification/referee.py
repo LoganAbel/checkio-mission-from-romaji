ad9ac6ba8f3c527911b01380201ca9324b4c93a4
LoganAbel <106107469+LoganAbel@users.noreply.github.com>
2024-05-21 18:46:35 -0500

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -11,8 +11,8 @@ api.add_listener(
         tests=TESTS,
         # checker=to_list,
         function_name={
-            "python": "sum_two",
-            "js": "sumTwo"
+            "python": "from_romanji",
+            "js": "fromRomanji"
         },
         cover_code={
             'python-3': {},
